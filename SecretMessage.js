a7ae6bd3af5f16064ffe678a792bc81c3e43696e
beylar <beylaruz@gmail.com>
2024-02-25 16:14:18 +0200

    Adding words to the message

--- SecretMessage.js
+++ SecretMessage.js
@@ -3,8 +3,18 @@ let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'ea
 //find the length of the array
 console.log(secretMessage.length);
 //remove the last element of the array
-console.log(secretMessage);
+
+//console.log(secretMessage);
 secretMessage.pop();
 //after removing the last element
+
+
+//enter the elements
+secretMessage.push('to', 'Program');
+
+//finding the index of word easily in the message
+let easilyIndex = secretMessage.indexOf('easily');
+//replace it by right
+secretMessage[easilyIndex] = 'right';
 console.log(secretMessage);
 
